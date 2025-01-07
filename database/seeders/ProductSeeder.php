354880e851e772fdb47071d22b76f94072a122e7
DevBhagat-dash <Devkumar.B@dashtechinc.com>
2025-01-07 15:27:45 +0530

    seed file changes

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -14,18 +14,35 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
+        // Create example products
         Product::create([
             'sku' => 'PROD-1234-RED-M',
-            'name' => 'Red T-shirt',
+            'name' => 'Red T-Shirt',
             'image' => 'path_to_image',
-            'is_active' => true
+            'is_active' => true,
+            'category_id' => 2, // ID for Clothing
+            'sub_category_id' => 4, // ID for Women\'s Clothing
+            'details' => 'A comfortable red t-shirt made of cotton fabric.',
         ]);
 
         Product::create([
-            'sku' => 'PROD-5678-BLUE-L',
-            'name' => 'Blue Jeans',
+            'sku' => 'PROD-5678-BLACK-L',
+            'name' => 'Black Jacket',
             'image' => 'path_to_image',
-            'is_active' => true
+            'is_active' => true,
+            'category_id' => 2, // ID for Clothing
+            'sub_category_id' => 4, // ID for Women\'s Clothing
+            'details' => 'A stylish black jacket with a zipper closure.',
+        ]);
+
+        Product::create([
+            'sku' => 'PROD-9012-SILVER-16GB',
+            'name' => 'Silver Laptop 16GB',
+            'image' => 'path_to_image',
+            'is_active' => true,
+            'category_id' => 1, // ID for Electronics
+            'sub_category_id' => 2, // ID for Laptops
+            'details' => 'A powerful laptop with 16GB RAM and 512GB SSD.',
         ]);
     }
 }
--- database/seeders/ProductVariantSeeder.php
+++ database/seeders/ProductVariantSeeder.php
@@ -14,16 +14,49 @@ class ProductVariantSeeder extends Seeder
      */
     public function run()
     {
+
         ProductVarient::create([
-            'product_id' => 1, // Product ID for Red T-shirt
-            'unit' => 'M',
-            'price' => 19.99
+            'product_id' => 3, // Product ID for Red T-Shirt
+            'unit' => 'Small',
+            'price' => 19.99,
         ]);
 
         ProductVarient::create([
-            'product_id' => 2, // Product ID for Blue Jeans
-            'unit' => 'L',
-            'price' => 39.99
+            'product_id' => 3, // Product ID for Red T-Shirt
+            'unit' => 'Medium',
+            'price' => 19.99,
+        ]);
+
+        ProductVarient::create([
+            'product_id' => 3, // Product ID for Red T-Shirt
+            'unit' => 'Large',
+            'price' => 19.99,
+        ]);
+
+        // Create variants for the "Black Jacket"
+        ProductVarient::create([
+            'product_id' => 4, // Product ID for Black Jacket
+            'unit' => 'Small',
+            'price' => 59.99,
+        ]);
+
+        ProductVarient::create([
+            'product_id' => 4, // Product ID for Black Jacket
+            'unit' => 'Medium',
+            'price' => 59.99,
+        ]);
+
+        ProductVarient::create([
+            'product_id' => 4, // Product ID for Black Jacket
+            'unit' => 'Large',
+            'price' => 59.99,
+        ]);
+
+        // Create variants for the "Silver Laptop 16GB"
+        ProductVarient::create([
+            'product_id' => 5, // Product ID for Silver Laptop 16GB
+            'unit' => '16GB RAM, 512GB SSD',
+            'price' => 999.99,
         ]);
     }
 }
