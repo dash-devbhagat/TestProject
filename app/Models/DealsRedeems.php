529de693882a3971482caa8e911267d0f2819ffd
DevBhagat-dash <Devkumar.B@dashtechinc.com>
2025-02-04 16:26:14 +0530

    deals table chnages

--- app/Models/DealsRedeems.php
+++ app/Models/DealsRedeems.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Redeem extends Model
+class DealsRedeems extends Model
 {
     use HasFactory;
 
--- database/migrations/2025_02_04_093347_create_redeems_table.php
+++ database/migrations/2025_02_04_093347_create_redeems_table.php
@@ -4,18 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('redeems', function (Blueprint $table) {
+        Schema::create('deals_redeems', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('deal_id');
             $table->unsignedBigInteger('user_id');
             $table->boolean('is_redeemed')->default(false);
+            $table->foreignId('order_id')->nullable()->constrained('orders')->onDelete('set null');
+            $table->timestamp('used_at')->useCurrent();
             $table->timestamps();
 
             $table->foreign('deal_id')->references('id')->on('deals')->onDelete('cascade');
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('redeems');
+        Schema::dropIfExists('deals_redeems');
     }
 };
